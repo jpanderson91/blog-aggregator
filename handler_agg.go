f6b439068a539e6ca3c49d6e6886ca9596612807
John_Anderson <John.anderson@lsgskychefs.com>
2026-07-01 12:35:04 -0500

    added fetching

--- handler_agg.go
+++ handler_agg.go
@@ -3,13 +3,40 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.Args) != 1 {
+    return fmt.Errorf("usage: agg <time_between_reqs>")
 	}
-	fmt.Printf("%+v\n", feed)
-	return nil
+	duration, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't parse duration: %w", err)
+	}
+	fmt.Printf("Collecting feeds every %s\n", duration)
+	ticker := time.NewTicker(duration)
+	for ; ; <-ticker.C {
+    scrapeFeeds(s)
+	}
+
+}
+func scrapeFeeds(s *state) error {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+if err != nil {
+    return err
+}
+	err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		return err
+	}
+
+	fetched, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return err
+	}
+	for _, item := range fetched.Channel.Item {
+    fmt.Println(item.Title)
+}
+return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,3 +10,11 @@ INNER JOIN users ON feeds.user_id = users.id;
 
 -- name: GetFeedByURL :one
 SELECT * FROM feeds WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds SET updated_at = NOW(), last_fetched_at = NOW() WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at NULLS FIRST 
+LIMIT 1;
